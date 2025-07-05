6cf9d48bdc79ae2ff8794d91e7a748a2569c8287
unknown <you@example.com>
2025-07-05 11:21:01 +0530

    created frontend and backend route

---
+++ src/app/login/page.tsx
@@ -0,0 +1,8 @@
+export default function LoginPage() {
+    return (
+        <div className="flex flex-col text-black bg-white">
+            <h1>login</h1>
+        </div>
+
+    )
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,8 @@
+export default function LoginPage() {
+    return (
+        <div className="flex flex-col text-white bg-red-400 text-center  justify-center items-center w-full h-screen">
+            <h1>signup</h1>
+        </div>
+
+    )
+}
