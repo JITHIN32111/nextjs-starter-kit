05872180ca28d9375d7be389c2d41ba13e536a78
unknown <you@example.com>
2025-07-06 10:38:08 +0530

    created params page

---
+++ src/app/profile/[id]/page.tsx
@@ -0,0 +1,12 @@
+const userProfile = ({params}:any) => {
+
+    return (
+        <div className="text-black bg-white text-2xl">
+            profile {params.id}
+        </div>
+
+    )
+
+
+}
+export default userProfile
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,16 @@
+
+
+
+
+const profilePage = () => {
+
+    return (
+        <div className="text-black bg-white text-2xl">
+            profile
+        </div>
+
+    )
+
+
+}
+export default profilePage
